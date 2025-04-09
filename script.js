da914464cb8b5ac487cb12c17fc2acabcdc1f6a2
uneedelay <XenonPilot@yandex.ru>
2025-04-10 01:17:26 +0500

    Start

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.nav__link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
