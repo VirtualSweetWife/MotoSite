c4ac7a02d77f0814abb571b71ad346b6929fac34
uneedelay <XenonPilot@yandex.ru>
2025-05-20 18:00:35 +0500

    20052025

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ document.querySelectorAll('.nav__link').forEach(anchor => {
         });
     });
 });
+document.querySelectorAll('.btn').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
